212e859f675dfeb620e43d18fcd99d2bb20ab2b2
Humberto Ribeiro <humbertoemaus@gmail.com>
2025-01-14 15:29:29 -0300

    update info social media

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,6 +1,10 @@
 import Image from 'next/image';
 import Link from 'next/link';
-import { Instagram } from 'react-feather';
+import {
+    Instagram,
+    Mail,
+    MessageCircle
+} from 'react-feather';
 
 export default function FooterComponent() {
     return (
@@ -26,21 +30,31 @@ export default function FooterComponent() {
                 <p>Olinda - PE, CEP - 53140-080 - BR.</p>
             </address>
             <div className='flex flex-col items-center justify-center gap-1'>
-                <p>Contato</p>
-                <p>Telefone: (81) 9 9999-9999</p>
-                <p>Email: cleciosalba@gmail.com</p>
-                <div className='flex gap-2'>
-                    <p>
-                        Siga-nos
-                    </p>
-                    <Link
-                        className='text-fuchsia-600 hover:text-fuchsia-400 duration-300'
-                        href='https://www.instagram.com/dolbytec/'
-                        target='_blank'
-                    >
-                        <Instagram />
-                    </Link>
-                </div>
+                <p>Contato:</p>
+                <Link
+                    className='flex items-center gap-2 hover:text-green-400 duration-300'
+                    href='https://wa.me/5581988339798'
+                    target='_blank'
+                >
+                    <MessageCircle />
+                    <p>WhatsApp: (81) 9 8833-9798</p>
+                </Link>
+                <Link
+                    className='flex items-center gap-2 hover:text-blue-400 duration-300'
+                    href='mailto:cleciosalba@gmail.com'
+                    target='_blank'
+                >
+                    <Mail />
+                    <p>Email: cleciosalba@gmail.com</p>
+                </Link>
+                <Link
+                    className='flex gap-2 hover:text-fuchsia-400 duration-300'
+                    href='https://www.instagram.com/dolbytec/'
+                    target='_blank'
+                >
+                    <p>Siga-nos</p>
+                    <Instagram className='text-fuchsia-600 hover:text-fuchsia-400 duration-300' />
+                </Link>
             </div>
         </footer>
     );
